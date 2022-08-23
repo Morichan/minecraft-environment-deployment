5220a87f4769c908df5392da28ad009cb43a486c
Morichan <nintendo698@gmail.com>
2022-08-23 23:32:54 +0900

    ✨ feat(server.properties): max-tick-timeの無効化
    以下のような記事によると、Linux OSのWatchdogにより、Minecraftサーバー
    プロセスがちょっとしたWarningレベルに反応して落とされることが原因との
    こと。
    恐らく根本解決ではないと思うが、他に倣いmax-tick-timeを無効化すること
    で対応する。
    
    - http://ysh-urasoe.seesaa.net/article/417015073.html
    - https://github.com/blueb901/linux/issues/5

--- run_minecraft_server.sh
+++ run_minecraft_server.sh
@@ -2,6 +2,10 @@
 
 aws s3 sync s3://${BUCKET_NAME}/backup/latest/world/ world/
 
+# Change settings
+sed -i "s/max-tick-time=[0-9]*/max-tick-time=-1/g" server.properties
+
+
 trap "./terminate_minecraft_server.sh" EXIT
 
 java -Xmx1024M -Xms1024M -jar server.jar nogui

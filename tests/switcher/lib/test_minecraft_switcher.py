9ec1e02df6378a7eba66b8e786b3268681137a51
Morichan <nintendo698@gmail.com>
2022-09-21 18:40:15 +0900

    ♻️ refactor(test): テストコードでないメソッドのプライベート化

--- tests/switcher/lib/test_minecraft_switcher.py
+++ tests/switcher/lib/test_minecraft_switcher.py
@@ -14,13 +14,13 @@ with mock_cloudformation():
 
 @mock_cloudformation
 class TestMinecraftSwitcher:
-    def create_cfn_parameters(self, parameters, is_reused=None):
+    def _create_cfn_parameters(self, parameters, is_reused=None):
         return [({
             'ParameterKey': k,
             'ParameterValue': v
         } | ({'UsePreviousValue': is_reused} if is_reused is not None else {})) for k, v in parameters.items()]
 
-    def create_cfn_template_body(self, parameters):
+    def _create_cfn_template_body(self, parameters):
         return {
             'AWSTemplateFormatVersion': '2010-09-09',
             'Parameters': {k: {'Type': 'String'} for k in parameters.keys()},
@@ -34,7 +34,7 @@ class TestMinecraftSwitcher:
             },
         }
 
-    def create_cfn_stack(self, stack_name, parameters={}):
+    def _create_cfn_stack(self, stack_name, parameters={}):
         cfn = boto3.client('cloudformation')
 
         with mock_s3():
@@ -42,8 +42,8 @@ class TestMinecraftSwitcher:
             # https://github.com/spulec/moto/issues/4925
             cfn.create_stack(
                 StackName=stack_name,
-                TemplateBody=json.dumps(self.create_cfn_template_body(parameters)),
-                Parameters=self.create_cfn_parameters(parameters)
+                TemplateBody=json.dumps(self._create_cfn_template_body(parameters)),
+                Parameters=self._create_cfn_parameters(parameters)
             )
 
     def test_not_set_param_if_stack_name_is_null(self):
@@ -61,9 +61,9 @@ class TestMinecraftSwitcher:
     def test_get_cloudformation_one_parameter(self):
         stack_name = 'SampleStack'
         stack_params = {'SampleKey': 'SampleValue'}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(stack_params)
+        expected = self._create_cfn_parameters(stack_params)
 
         actual = obj.get_cloudformation_parameters()
 
@@ -72,9 +72,9 @@ class TestMinecraftSwitcher:
     def test_get_cloudformation_multi_parameters(self):
         stack_name = 'SampleStack'
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(stack_params)
+        expected = self._create_cfn_parameters(stack_params)
 
         actual = obj.get_cloudformation_parameters()
 
@@ -83,9 +83,9 @@ class TestMinecraftSwitcher:
     def test_not_get_cloudformation_empty_parameters(self):
         stack_name = 'SampleStack'
         stack_params = {}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(stack_params)
+        expected = self._create_cfn_parameters(stack_params)
 
         actual = obj.get_cloudformation_parameters()
 
@@ -93,7 +93,7 @@ class TestMinecraftSwitcher:
 
     def test_not_get_cloudformation_if_try_to_search_different_stack_name(self):
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
-        self.create_cfn_stack('SampleStack', stack_params)
+        self._create_cfn_stack('SampleStack', stack_params)
         obj = MinecraftSwitcher('NotFoundStackName')
 
         actual = obj.get_cloudformation_parameters()
@@ -104,9 +104,9 @@ class TestMinecraftSwitcher:
         stack_name = 'SampleStack'
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
         overrode_params = {'SampleKey': 'Overrode', 'ExampleKey': 'Rewritten'}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(overrode_params)
+        expected = self._create_cfn_parameters(overrode_params)
 
         obj.update_cloudformation_stack(overrode_params)
         actual = obj.get_cloudformation_parameters()
@@ -118,9 +118,9 @@ class TestMinecraftSwitcher:
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
         overrode_params = {'SampleKey': 'Overrode'}
         ignore_overrode_params = {'ExampleKey': 'ExampleValue'}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(overrode_params | ignore_overrode_params)
+        expected = self._create_cfn_parameters(overrode_params | ignore_overrode_params)
 
         obj.update_cloudformation_stack(overrode_params)
         actual = obj.get_cloudformation_parameters()
@@ -131,9 +131,9 @@ class TestMinecraftSwitcher:
         stack_name = 'SampleStack'
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
         overrode_params = {}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(stack_params)
+        expected = self._create_cfn_parameters(stack_params)
 
         with pytest.raises(UnnecessaryToUpdateStackError):
             obj.update_cloudformation_stack(overrode_params)
@@ -142,9 +142,9 @@ class TestMinecraftSwitcher:
         stack_name = 'SampleStack'
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
         overrode_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
-        self.create_cfn_stack(stack_name, stack_params)
+        self._create_cfn_stack(stack_name, stack_params)
         obj = MinecraftSwitcher(stack_name)
-        expected = self.create_cfn_parameters(overrode_params)
+        expected = self._create_cfn_parameters(overrode_params)
 
         with pytest.raises(UnnecessaryToUpdateStackError):
             obj.update_cloudformation_stack(overrode_params)
@@ -152,9 +152,9 @@ class TestMinecraftSwitcher:
     def test_raise_exception_when_updated_cloudformation_if_is_not_found(self):
         stack_params = {'SampleKey': 'SampleValue', 'ExampleKey': 'ExampleValue'}
         overrode_params = {}
-        self.create_cfn_stack('SampleStack', stack_params)
+        self._create_cfn_stack('SampleStack', stack_params)
         obj = MinecraftSwitcher('NotFoundStackName')
-        expected = self.create_cfn_parameters(stack_params)
+        expected = self._create_cfn_parameters(stack_params)
 
         with pytest.raises(NotFoundStackError):
             obj.update_cloudformation_stack(overrode_params)
